1991761920be952ac5a36023f0cae5d395911797
jahnathan x <jahnathanexantus@gmail.com>
2022-04-05 22:54:09 -0400

    created startgame function

--- asset/script.js
+++ asset/script.js
@@ -1,3 +1,34 @@
 // create question
 var question = document.getElementById('question-prop');
-var button = document.getElementById('start-btn');
+var startButton = document.getElementById('start-btn');
+var btn1 = document.getElementById('button-1');
+var btn2 = document.getElementById('button-2');
+var btn3 = document.getElementById('button-3');
+var btn4 = document.getElementById('button-4');
+var secondLt = 0;
+var timerEl = 10;
+var timerId = document.getElementById('timerId')
+
+
+
+
+startButton.addEventListener('click',function(event){
+    event.preventDefault();
+    startGame();
+});
+
+
+
+function startGame () {
+console.log('started');
+}
+
+function timer() {
+    timerEl= setInterval(secondLt--,1000);
+    timerEl.textcontext = secondLt
+    if(secondLt == 0){
+        clearInterval(interval)
+    }
+
+        
+    }
